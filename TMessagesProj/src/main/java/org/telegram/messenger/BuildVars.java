5e533248e880abe1ba7db9ab1d7f26e7cf86c464
xushihai <1056483075@qq.com>
2017-03-08 14:10:50 +0800

    修改readme

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -0,0 +1,41 @@
+/*
+ * This is the source code of Telegram for Android v. 3.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2016.
+ */
+
+package org.telegram.messenger;
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = true;
+    public static int BUILD_VERSION = 821;
+    public static String BUILD_VERSION_STRING = "3.10";
+    public static int APP_ID = 65222; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "ff570bc43b9ec4d99b1a0ea74c31fc2d"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "" +
+            "ff570bc43b9ec4d99b1a0ea74c31fc2d";
+    public static String HOCKEY_APP_HASH_DEBUG = "" +
+            "ff570bc43b9ec4d99b1a0ea74c31fc2d";
+    public static String GCM_SENDER_ID = "760348033672";
+    public static String SEND_LOGS_EMAIL = "1056483075@qq.com";
+    public static String BING_SEARCH_KEY = "-----BEGIN RSA PUBLIC KEY-----\n" +
+            "MIIBCgKCAQEAwVACPi9w23mF3tBkdZz+zwrzKOaaQdr01vAbU4E1pvkfj4sqDsm6\n" +
+            "lyDONS789sVoD/xCS9Y0hkkC3gtL1tSfTlgCMOOul9lcixlEKzwKENj1Yz/s7daS\n" +
+            "an9tqw3bfUV/nqgbhGX81v/+7RFAEd+RwFnK7a+XYl9sluzHRyVVaTTveB2GazTw\n" +
+            "Efzk2DWgkBluml8OREmvfraX3bkHZJTKX4EQSjBbbdJ2ZXIsRrYOXfaA+xayEGB+\n" +
+            "8hdlLmAjbCVfaigxX0CDqWeR1yFL9kwd9P0NsZRPsmoqVwMbMu7mStFai6aIhc3n\n" +
+            "Slv8kg9qv1m6XHVQY3PnEw+QQtqSIXklHwIDAQAB\n" +
+            "-----END RSA PUBLIC KEY-----"; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = "-----BEGIN RSA PUBLIC KEY-----\n" +
+            "MIIBCgKCAQEAwVACPi9w23mF3tBkdZz+zwrzKOaaQdr01vAbU4E1pvkfj4sqDsm6\n" +
+            "lyDONS789sVoD/xCS9Y0hkkC3gtL1tSfTlgCMOOul9lcixlEKzwKENj1Yz/s7daS\n" +
+            "an9tqw3bfUV/nqgbhGX81v/+7RFAEd+RwFnK7a+XYl9sluzHRyVVaTTveB2GazTw\n" +
+            "Efzk2DWgkBluml8OREmvfraX3bkHZJTKX4EQSjBbbdJ2ZXIsRrYOXfaA+xayEGB+\n" +
+            "8hdlLmAjbCVfaigxX0CDqWeR1yFL9kwd9P0NsZRPsmoqVwMbMu7mStFai6aIhc3n\n" +
+            "Slv8kg9qv1m6XHVQY3PnEw+QQtqSIXklHwIDAQAB\n" +
+            "-----END RSA PUBLIC KEY-----"; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = "65222"; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_VERSION = "20150326";
+}
